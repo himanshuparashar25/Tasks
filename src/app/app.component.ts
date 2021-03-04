e41aff0aba48d7b926b31f54f413011042da75b0
himanshu.p <himanshu.p@engineerbabu.in>
2021-03-04 23:25:21 +0530

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'addwith';
+  firstNum :string;
+  SecondNum :string;
+  result: number;
+
+  addNumber(){
+    this.result == parseInt(this.firstNum) + parseInt(this.SecondNum);
+  }
+  subNumber(){
+    this.result == parseInt(this.firstNum) - parseInt(this.SecondNum);
+  }
+  multiplyNumber(){
+    this.result == parseInt(this.firstNum) * parseInt(this.SecondNum);
+  }
+  divideNumber(){
+    this.result == parseInt(this.firstNum) / parseInt(this.SecondNum);
+  }
+  percentNumber(){
+    this.result == parseInt(this.firstNum) % parseInt(this.SecondNum);
+  
 }
